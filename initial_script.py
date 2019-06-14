00bb1eb2bc395daacea5c40d7a3b976db642b118
BenedictusAryo <benedictusaryo@outlook.com>
2019-06-14 10:49:54 +0700

    add IHSG symbol

--- initial_script.py
+++ initial_script.py
@@ -1,1 +1,3 @@
-data.DataReader('BBCA.JK', 'av-daily', start_date, end_date, access_key='1NXT558D1K9RTUR3') 
+data.DataReader('BBCA.JK', 'av-daily', start_date, end_date, access_key='1NXT558D1K9RTUR3')
+
+data.DataReader('^JKSE', 'av-daily', start_date, end_date, access_key='1NXT558D1K9RTUR3') 
